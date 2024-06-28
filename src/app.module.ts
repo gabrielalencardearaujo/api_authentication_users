4d1e34f7f1dcd92e2c3acd014f57a4abce4288b9
Galencar <dev.alencar.gabriel@gmail.com>
2024-06-27 21:42:57 -0300

    chore: add typeorm and set your config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     TaskModule,
     UsersModule,
     AuthModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
